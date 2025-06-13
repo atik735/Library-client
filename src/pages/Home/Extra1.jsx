cc7c634771508b793ee99afe6b0966cfbf811a60
Atik Hassan <atikh01001@gmail.com>
2025-06-13 18:14:08 +0600

    extra 1section completed

--- src/pages/Home/Extra1.jsx
+++ src/pages/Home/Extra1.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import extra1 from '../../assets/extra1.jpg'
 const Extra1 = () => {
     return (
+        <div className='bg-slate-200 flex items-center justify-evenly p-20'>
+
         <div>
-            
+            <img src={extra1} className='w-96 rounded' alt="Books" />
+        </div>
+
+        <div className='text-center space-y-16'>
+
+        <section className='space-y-4'>
+        <h1 className='font-bold text-4xl text-gray-700'>STAY WITH US</h1>
+        <p>Subscribe to our newsletters now and stay up-to-date with new collections, <br /> the latest lookbooks and exclusive offers.</p>
+        </section>
+
+        <form className='flex relative items-center'>
+            <input type="email" name="email" className='outline-none w-full border-0 border-b' placeholder='Enter your e-mail' />
+
+            <input type="submit" value="SUBSCRIBE" className='font-bold cursor-pointer relative right-22 text-lg text-gray-700' />
+
+        </form>
+
+        </div>
+
         </div>
     );
 };
